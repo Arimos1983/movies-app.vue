387fabdef8089ef60bd660f37631a36bcc3b9a6d
Arimos <amilinkovic.ns@gmail.com>
2018-08-21 18:54:52 +0200

    zadatak 2 i 3

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,12 +1,28 @@
 <template>
   <div >
-    
+      <ul v-for="movie in movies" :key="movie.id">
+        <li>{{movie.title}}</li>
+      </ul>
   </div>
 </template>
 
 <script>
+import { movies } from '../services/Movies.js'
 export default {
-  name: 'HelloWorld',
+  name: 'AppMovies',
+  data(){
+    return {
+      movies: {}
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    movies.getAll()
+      .then((response) => {
+          next((vm) => {
+            vm.movies = response.data
+          })
+      })
+  },
   
 }
 </script>
