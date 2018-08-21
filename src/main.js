058ed28d2f26684a39d40a81575280c9255eb9dc
Arimos <amilinkovic.ns@gmail.com>
2018-08-21 18:32:26 +0200

    zadatak 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Movies from './components/AppMovies'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', redirect: '/movies' },
+  {path: '/movies', component: Movies}
+  
+  
+
+]
+
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
